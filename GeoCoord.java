479c9c36c71fd024fcfc0f7eecacae92b28bc794
meeshic <michellecsr@gmail.com>
2018-02-28 00:50:52 -0800

    implemented compileable GeoCoord class (C++->Java)

---
+++ GeoCoord.java
@@ -0,0 +1,30 @@
+public class GeoCoord implements Comparable<GeoCoord>
+{
+    double latitude;
+	double longitude;
+	String sLatitude;
+    String sLongitude;
+	GeoCoord(String latitude, String longitude)
+	{
+		this.latitude = Double.parseDouble(latitude);
+		this.longitude = Double.parseDouble(longitude);
+
+		sLatitude = latitude;
+		sLongitude = longitude;
+	}
+    
+    public boolean equals(Object o){
+        GeoCoord c = (GeoCoord) o;
+        
+        return (this.latitude == c.latitude) && (this.longitude == c.longitude);
+    
+    }
+    
+    public int compareTo(GeoCoord c){
+        if(this.latitude < c.latitude) return -1;
+        if(this.latitude > c.latitude) return +1;
+        if(this.longitude < c.longitude) return -1;
+        if(this.longitude > c.longitude) return +1;
+        return 0;        
+    }
+}
