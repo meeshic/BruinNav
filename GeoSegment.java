567b005adeaaf9fb4f63f6f77ba3487a617cf79b
meeshic <michellecsr@gmail.com>
2018-02-28 00:51:27 -0800

    implemented compileable GeoSegment class (C++->Java)

---
+++ GeoSegment.java
@@ -0,0 +1,21 @@
+public class GeoSegment
+{
+    GeoCoord start;
+	GeoCoord end;
+    
+    GeoSegment(){}
+    
+    GeoSegment(GeoCoord s, GeoCoord e){
+        start = s;
+        end = e;
+    }
+    
+    public String toString(){
+        String startSeg = "start: " + start.sLatitude + ", end: " + start.sLongitude; 
+        String endSeg = "start: " + end.sLatitude + ", end: " + end.sLongitude; 
+        
+        String result = startSeg + " " + endSeg;
+        
+        return result;
+    }
+}
